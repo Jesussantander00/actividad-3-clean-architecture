b91489a5a8f08487d54bc91f6033941f85852031
Jesús Daniel Quintana Santander <jesusdanielquintanasantander@gmail.com>
2025-07-04 14:21:16 -0500

    Primer commit: conexión del proyecto con GitHub

---
+++ src/main/java/com/mycompany/actividad4/Actividad4.java
@@ -0,0 +1,9 @@
+package com.mycompany.actividad4;
+
+public class Actividad4 {
+
+    public static void main(String[] args) {
+        System.out.println("✅ ¡Hola mundo desde Maven!");
+    }
+}
+  
